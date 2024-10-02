b833cd574c7ec1cf8908b9575b31997814d68acd
lepsanovic <stefanlepsanovic@gmail.com>
2024-10-02 22:38:01 +0200

    Init product service

---
+++ product-service/src/test/java/com/order_app/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order_app.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
